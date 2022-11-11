295c6df80b61cf3730e337d2a9ed8dfb6b2ee99d
Adrian <adrianvalverde45@gmail.com>
2022-11-11 11:59:51 -0500

    added json server and also corrected html labels

--- project.js
+++ project.js
@@ -1,1 +1,7 @@
-console.log("hello world!")
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Dom has loaded")
+
+    const yourNameInput = document.querySelector("#form")
+    console.log(yourNameInput)
+})
+
